49571ce5fb271b86ce7d720f9631142971786449
sayn0s <ssyouth0717@gmail.com>
2024-03-24 14:55:26 +0900

    refactor: 余計なフェッチをなくす対応

--- workspaces/app/src/features/ranking/components/RankingCard.tsx
+++ workspaces/app/src/features/ranking/components/RankingCard.tsx
@@ -1,5 +1,6 @@
-import { Suspense } from 'react';
 import styled from 'styled-components';
+
+import type { GetBookResponse } from '@wsh-2024/schema/src/api/books/GetBookResponse';
 
 import { SvgIcon } from '../../../features/icons/components/SvgIcon';
 import { Box } from '../../../foundation/components/Box';
@@ -11,7 +12,6 @@ import { Spacer } from '../../../foundation/components/Spacer';
 import { Text } from '../../../foundation/components/Text';
 import { useImage } from '../../../foundation/hooks/useImage';
 import { Color, Radius, Space, Typography } from '../../../foundation/styles/variables';
-import { useBook } from '../../book/hooks/useBook';
 
 const _Wrapper = styled.li`
   width: 100%;
@@ -38,12 +38,10 @@ const _AvatarWrapper = styled.div`
 `;
 
 type Props = {
-  bookId: string;
+  book: Omit<GetBookResponse, 'nameRuby'>;
 };
 
-const RankingCard: React.FC<Props> = ({ bookId }) => {
-  const { data: book } = useBook({ params: { bookId } });
-
+export const RankingCard: React.FC<Props> = ({ book }) => {
   const imageUrl = useImage({ height: 96, imageId: book.image.id, width: 96 });
   const authorImageUrl = useImage({ height: 32, imageId: book.author.image.id, width: 32 });
 
@@ -102,13 +100,3 @@ const RankingCard: React.FC<Props> = ({ bookId }) => {
     </_Wrapper>
   );
 };
-
-const RankingCardWithSuspense: React.FC<Props> = (props) => {
-  return (
-    <Suspense fallback={null}>
-      <RankingCard {...props} />
-    </Suspense>
-  );
-};
-
-export { RankingCardWithSuspense as RankingCard };
